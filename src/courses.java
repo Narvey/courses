6f691948b8343f9e2ce4ec174deadb0e51552f7c
N Harvey <nharvey@mymail.mines.edu>
2012-10-02 09:08:49 -0600

    empty shell of class

---
+++ src/courses.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class courses {
+	private Map<String, String> courses;
+
+	public courses() {
+		courses = new HashMap<String, String>();
+	}
+	public static void main(String[] args){
+		//TODO fill with loop and other requirements
+	}
+}
